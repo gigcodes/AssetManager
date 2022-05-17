9e9fa48f3e6c7698b90ada3d0c1d8dcc8e7b2e23
Karthick K <karthickk1996@hotmail.com>
2022-05-17 11:19:31 +0530

    feat: Rewamped all.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\MediaController;
-use App\Http\Controllers\UploadFileController;
+use Gigcodes\AssetManager\Http\Controllers\MediaController;
+use Gigcodes\AssetManager\Http\Controllers\UploadFileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
