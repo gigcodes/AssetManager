036a08f45dbbb2d384cc32f02cae2498d447ab31
Karthick K <karthickk1996@hotmail.com>
2022-05-17 13:30:46 +0530

    fix: issues with asset manager migrations

--- src/AssetManagerServiceProvider.php
+++ src/AssetManagerServiceProvider.php
@@ -27,14 +27,21 @@ class AssetManagerServiceProvider extends ServiceProvider
             __DIR__ . '/../config/asset_manager.php' => config_path('asset_manager.php'),
         ], 'config');
 
-        if (!class_exists('CreateMediaTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_media_collections_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_collections_table.php'),
-                __DIR__ . '/../database/migrations/create_media_folders_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_folders_table.php'),
-                __DIR__ . '/../database/migrations/create_media_files_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_files_table.php'),
-                __DIR__ . '/../database/migrations/create_exif_metas_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_exif_metas_table.php'),
-            ], 'migrations');
-        }
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_media_collections_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_collections_table.php'),
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_media_folders_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_folders_table.php'),
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_media_files_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_files_table.php'),
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_exif_metas_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_exif_metas_table.php'),
+        ], 'migrations');
 
         $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
     }
