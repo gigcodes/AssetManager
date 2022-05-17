09c7b8634dc25314669b2b23c04b45e1c17d998f
Karthick K <karthickk1996@hotmail.com>
2022-05-17 13:34:33 +0530

    fix: issues with asset manager migrations

--- src/AssetManagerServiceProvider.php
+++ src/AssetManagerServiceProvider.php
@@ -28,19 +28,19 @@ class AssetManagerServiceProvider extends ServiceProvider
         ], 'config');
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_media_collections_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_collections_table.php'),
+            __DIR__ . '/../database/migrations/create_media_collections_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 30) . '_create_media_collections_table.php'),
         ], 'migrations');
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_media_folders_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_folders_table.php'),
+            __DIR__ . '/../database/migrations/create_media_folders_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 60) . '_create_media_folders_table.php'),
         ], 'migrations');
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_media_files_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_files_table.php'),
+            __DIR__ . '/../database/migrations/create_media_files_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 90) . '_create_media_files_table.php'),
         ], 'migrations');
 
         $this->publishes([
-            __DIR__ . '/../database/migrations/create_exif_metas_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_exif_metas_table.php'),
+            __DIR__ . '/../database/migrations/create_exif_metas_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 120) . '_create_exif_metas_table.php'),
         ], 'migrations');
 
         $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
